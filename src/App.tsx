dc18981126a2483f47eecf07f2b3eb494a000e50
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-22 14:54:41 +0530

    Router and link finalize

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,20 @@ import './App.css'
 import Header from "./components/layout/Header.tsx";
 import Footer from "./components/layout/footer.tsx";
 import Home from "./views/home.tsx";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 
 
     return (
         <div>
-            {/*header*/}
-
-         <Header/>
-
-            {/*main*/}
-            <section>
-                <Home/>
-            {/*    <div className={'grid 2xl:grid-cols-5   xl:grid-cols-4 lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-2 w-fit relative m-auto'}>
-
-                       <Card/>
-                       <Card/>
-                       <Card/>
-                       <Card/>
-                       <Card/>
-                       <Card/>
-                       <Card/>
-                       <Card/>
-                </div>*/}
-
-
-
-            </section>
-
-            {/*footer*/}
-            <Footer/>
-
             <BrowserRouter>
+                <Header/>
+                    <Routes>
+                        <Route path={"/"} element={<Home/>}/>
+                    </Routes>
+
+                <Footer/>
             </BrowserRouter>
 
         </div>
